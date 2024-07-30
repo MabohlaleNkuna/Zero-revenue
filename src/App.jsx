2663ec5994c9d6b2e3b97651e337f5fb992fa690
MabohlaleNkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2024-07-30 09:47:41 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import React, { useState } from 'react';
 import Cards from './components/Cards';
 import myImage1 from './components/pic1.jpeg';
 import myImage2 from './components/pic.jpeg';
-import myImage3 from './components/pic2.jpeg';
+import myImage3 from './components/pic2.jpg';
 
 function App() {
   const [currentCard, setCurrentCard] = useState(0);
 
   const cardsData = [
     { title: "Zero", message: "2.45%", paragraph: "Average return-Value", imgSrc: myImage3, cardColor: "red" },
-    { title: "Zero", message: "Danny Hazelwood", paragraph: "Founder - Zeropay", imgSrc: myImage2, cardColor: "black", middleImage: true },
+    { title: "Zero", message: <>Danny <br /> Hazelwood</>, paragraph: "Founder - Zeropay", imgSrc: myImage2, cardColor: "black", middleImage: true },
     { title: "Zero", message: <>Boost<br /> Your Earning <br /> Today</>, paragraph: <>Growth <br /> From 2022 <br /> Today</>, symbol: "ℳ", imgSrc: myImage1, cardColor: "green" }
   ];
 
